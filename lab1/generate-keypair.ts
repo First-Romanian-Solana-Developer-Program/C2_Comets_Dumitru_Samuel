328172d8a2c6e321d44cc6cdc3394edf88c67dc1
Dumitru <samuel.dumitru0220@gmail.com>
2024-10-09 22:48:41 +0300

    small changes

--- lab1/generate-keypair.ts
+++ lab1/generate-keypair.ts
@@ -2,7 +2,7 @@ import {Keypair} from "@solana/web3.js";
 
 const keypair = Keypair.generate();
 
-console.log('generated keypair');
+console.log('generating keypair....');
 console.log(`the public key is : `, keypair.publicKey.toBase58());
 console.log(`the private key is : `, keypair.secretKey);
-console.log('generated keypair');
+console.log('Well done');
