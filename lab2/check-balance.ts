ba6e994c343472334b7b433e1401032445849fa7
Dumitru <samuel.dumitru0220@gmail.com>
2024-10-10 19:52:17 +0300

    lab2Finished

---
+++ lab2/check-balance.ts
@@ -0,0 +1,27 @@
+import "dotenv/config"
+import {Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js"
+
+import {airdropIfRequired} from "@solana-developers/helpers"
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+
+console.log("connected to devnet", connection.rpcEndpoint)
+
+const samipublicKey = new PublicKey("9HccTUdELCnUP9fPpv2i9gNcx5JJeQnpAxkwkqXE9SvK")
+
+const balanceInLamports = await connection.getBalance(samipublicKey);
+console.log("sami balance: ", balanceInLamports)
+
+// console.log("airdrop 1 sol to sami....")
+
+// await airdropIfRequired(
+//     connection,
+//     samipublicKey,
+//     1*LAMPORTS_PER_SOL,
+//     0.5*LAMPORTS_PER_SOL,
+
+// );
+
+console.log("airdrop done")
+
+const balanceInLamports2 = await connection.getBalance(samipublicKey);
+console.log("sami balance: ", balanceInLamports2)
